838497b4d3358c1a8475d08420357a9f30844976
Kayla <6lackmacaroon@gmail.com>
2019-05-28 15:41:48 -0600

    debugged async function error - annotated all request handlers

--- data/Express-Router.js
+++ data/Express-Router.js
@@ -4,42 +4,69 @@ const router = require('express').Router();
 
 
 //request handler functions
-
+//create new post
 router.post('/', async (req, res) => {
-      if(!req.body.title || !req.body.contents) {
-            res.status(400).json({ error: "So sorry, you must have both a title AND contents." })
-      } //400 = bad request
-      db
-            .insert()
-            .then(post => {
-                  res.status(201).json(post);
-            }) //201 = created
-            .catch(err => {
-                  res.status(500).json({ err: 'Could not create new post' })
-            })
+      try {
+            const posts = await db.insert(req.query);
+            if(!req.body.title || !req.body.contents) {
+                  res.status(400).json({ errorMessage: "So sorry, you must provide both the title AND contents." })
+            } else {
+                  res.status(200).json(posts);
+            }
+      } catch (err) {
+            res.status(500).json({ 
+                  error: "There was an error while saving the post to the database" 
+            });
+
+      }
 });
 
-//
+//create new post comment
 router.post('/:id/comments', (req, res) => {
 
 });
 
-router.get('/', (req, res) => {
+//find posts
+router.get('/', async (req, res) => {
+      try {
+            const posts= await db.find(req.query);
+            res.status(200).json(posts);
+      } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                  message: "Error retrieving your posts",
+            });
+      }
+});
+
+//find specific post by id
+router.get('/:id', async (req, res) => {
+      try {
+            const post = await db.findById(req.params.id);
+            if (post) {
+                  res.status(200).json(post);
+            } else {
+                  res.status(404).json({ message: "The post with the specified ID does not exist." });
+            }
+      } catch (err) {
+            res.status(500).json({
+                  error: "The post information could not be retrieved." 
+            })
+      }
 
 });
 
-router.get('/:id', (req, res) => {
-
-});
-
+//find comments for specific post
 router.get('/:id/comments', (req, res) => {
 
 });
 
+//delete specific post by id
 router.delete('/:id', (req, res) => {
 
 });
 
+//edit specific post by id
 router.put('/:id', (req, res) => {
 
 });
